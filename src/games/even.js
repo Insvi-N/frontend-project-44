f0d5e8e00c50fb727654e06dbebaa8518f303aff
Svetlana Nepomnyashchaya <lana.insvi@gmail.com>
2026-09-14 14:36:11 +0700

    #!/usr/bin/env node added

--- src/games/even.js
+++ src/games/even.js
@@ -1,6 +1,6 @@
 import runGame from '../index.js';
 
-const taskDescription = 'What is the result of the expression?'
+const taskDescription = 'Answer "yes" if the number is even, otherwise answer "no".'
 const gameMath = () => {
   const isEven = (number) => number % 2 === 0;
   const randomInt = Math.floor(Math.random()*100);
